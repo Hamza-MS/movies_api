5108f8183d704faeb25393af0e770b3ae968fe16
Hamza El mesaouab <77578145+Hamza-MS@users.noreply.github.com>
2023-10-01 14:52:23 +0100

    get all movies endpoint
    adding the getAllMovies endpoint : from repo to controller

--- src/main/java/dev/tut/movies/repositories/MovieRepository.java
+++ src/main/java/dev/tut/movies/repositories/MovieRepository.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface MovieRepository extends MongoRepository<Movie, ObjectId> {
 
