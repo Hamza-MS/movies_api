51f55c406860e96b867485cc9e794cb70cf526c1
Hamza El mesaouab <77578145+Hamza-MS@users.noreply.github.com>
2023-10-01 18:43:58 +0100

    add a review to a movie endpoint

--- src/main/java/dev/tut/movies/controllers/ReviewController.java
+++ src/main/java/dev/tut/movies/controllers/ReviewController.java
@@ -2,12 +2,21 @@ package dev.tut.movies.controllers;
 
 import dev.tut.movies.services.ReviewService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/reviews")
 public class ReviewController {
     @Autowired
     private ReviewService reviewService;
+
+    @PostMapping
+    public void createReviewForMovie(@RequestBody Map<String,String> payLoad){
+        reviewService.reviewMovie(payLoad.get("reviewBody"),payLoad.get("imdbId"));
+    }
 }
--- src/main/java/dev/tut/movies/services/ReviewService.java
+++ src/main/java/dev/tut/movies/services/ReviewService.java
@@ -1,11 +1,31 @@
 package dev.tut.movies.services;
-
+import dev.tut.movies.enteties.Movie;
+import dev.tut.movies.enteties.Review;
+import dev.tut.movies.repositories.MovieRepository;
 import dev.tut.movies.repositories.ReviewRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class ReviewService {
     @Autowired
     private ReviewRepository reviewRepository;
+
+    @Autowired
+    private MovieRepository movieRepository;
+
+    public void reviewMovie(String reviewBody, String imdbId){
+        Optional<Movie> optionalMovie = movieRepository.findByImdbId(imdbId);
+        Review review =reviewRepository.insert(new Review(reviewBody)) ;
+
+        if (optionalMovie.isPresent()){
+            Movie movie= optionalMovie.get();
+            movie.getReviewsId().add(review);
+            movieRepository.save(movie);
+
+        }
+
+    }
 }
